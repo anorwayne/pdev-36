348835750badc32155612ff2514c356931002f74
anorwayne <142409549+anorwayne@users.noreply.github.com>
2023-12-10 17:10:10 +1000

    NewsPortalUpdate
    /news/ | censor

--- news/models.py
+++ news/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
@@ -36,3 +37,4 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
+
--- news/views.py
+++ news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import News, get_object_or_404
 
-# Create your views here.
+
+def news_list(request):
+    news_list = News.objects.order_by('-date_published')
+    return render(request, 'news_list.html', {'news_list': news_list})
+
+
+def news_detail(request, news_id):
+    news = get_object_or_404(News, id=news_id)
+    return render(request, 'news_detail.html', {'news': news})
